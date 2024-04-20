bf8d6ae8745de7b97b9a5ad4c48c6079d42348aa
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-04-20 14:39:10 +0530

    Link sign-in page to footer link

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div >Footer</div>
+    <div >
+        <p className="font-sans">
+          Copyright <span>&#169;</span> 2024 - <Link to="/sign-in" className='text-blue-400'>_mr_glacier___</Link> - All
+          Rights Reserved
+        </p>
+    </div>
   )
 }
