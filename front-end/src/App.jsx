9f4c9fb2dcd4a86983bb6319fc07b87e0a45feca
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-02 17:33:24 +0530

    Make profile page private

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter,Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Edit from "./pages/Edit";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -8,7 +10,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/edit" element={<PrivateRoute />}>
+          <Route path="/edit" element={<Edit />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Footer() {
-  const {currentUser} = useSelector(state=>state.user);
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div >
-        <p className="font-sans">
-          Copyright <span>&#169;</span> 2024 - <Link to="/sign-in" className='text-blue-400'>_mr_glacier___</Link> - All
-          Rights Reserved
-        </p>
+    <div>
+      <p className="font-sans">
+        Copyright <span>&#169;</span> 2024 -{" "}
+        {currentUser ?(
+          <Link to="/edit" className="text-blue-400">
+          _mr_glacier___
+        </Link>
+        ) : (<Link to="/sign-in" className="text-blue-400">
+        _mr_glacier___
+      </Link>)}
+        {" "}
+        - All Rights Reserved
+      </p>
     </div>
-  )
+  );
 }
