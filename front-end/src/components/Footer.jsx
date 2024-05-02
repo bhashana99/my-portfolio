e68fe73db539813716d7c133a748c682351599f1
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-02 16:39:51 +0530

    add redux toolkit

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function Footer() {
+  const {currentUser} = useSelector(state=>state.user);
   return (
     <div >
         <p className="font-sans">
