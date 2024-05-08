b6c294855461b4aef025aefd25751ae7fcc56520
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-08 22:49:06 +0530

    Create pages route and Link  Edit page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Edit from "./pages/Edit";
+import BasicInfo from "./pages/BasicInfo";
+import SocialMedia from "./pages/SocialMedia";
+import Projects from "./pages/Projects";
+import Education from "./pages/Education";
+import Certificate from "./pages/Certificate";
+import Experience from "./pages/Experience";
+import Contact from "./pages/Contact";
 import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
@@ -12,6 +19,13 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route  element={<PrivateRoute />}>
           <Route path="/edit" element={<Edit />} />
+          <Route path="/basic-info" element={<BasicInfo />} />
+          <Route path="/social-media" element={<SocialMedia />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/education" element={<Education/>} />
+          <Route path="/certificate" element={<Certificate />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- front-end/src/pages/Edit.jsx
+++ front-end/src/pages/Edit.jsx
@@ -1,14 +1,68 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import { CiSettings } from "react-icons/ci";
+import { FaHandPointRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Edit() {
   return (
-    <div className="flex flex-col md:flex-row gap-3 bg-gray-500">
-        {/* side bar */}
-        
-         <Sidebar className="basis-1/3"/>
-        
-        <div >sc</div>
+    <div className="flex flex-col md:flex-row gap-3 bg-gray-300 min-h-screen">
+      {/* side bar */}
+      <div className="basis-1/3">
+        <Sidebar />
+      </div>
+      {/* welcome */}
+      <div className="flex-1 flex items-center justify-center h-screen">
+        <div className="flex flex-col items-center">
+          <CiSettings className="text-5xl mb-5 " />
+          <h1 className="font-bold font-serif">Hey..Welcome..!</h1>
+          <h1 className="mb-5">Let's Customize portfolio</h1>
+          <ul className="md:hidden sm:grid">
+            <Link to="/basic-info">
+              <li className="mt-5 flex flex-row gap-4 font-semibold items-center">
+                <FaHandPointRight className="text-xl" />
+                Basic Info
+              </li>
+            </Link>
+            <Link to="/social-media">
+              <li className="mt-5 flex flex-row gap-4 font-semibold items-center">
+                <FaHandPointRight className="text-xl" />
+                Social Media
+              </li>
+            </Link>
+            <Link to="/projects">
+              <li className="mt-5 flex flex-row gap-4 font-semibold items-center">
+                <FaHandPointRight className="text-xl" />
+                Projects
+              </li>
+            </Link>
+            <Link to="/education">
+              <li className="mt-5 flex flex-row gap-4 font-semibold items-center">
+                <FaHandPointRight className="text-xl" />
+                Education
+              </li>
+            </Link>
+            <Link to="/certificate">
+              <li className="mt-5 flex flex-row gap-4 font-semibold items-center">
+                <FaHandPointRight className="text-xl" />
+                Certificate
+              </li>
+            </Link>
+            <Link to="/experience">
+              <li className="mt-5 flex flex-row gap-4 font-semibold items-center">
+                <FaHandPointRight className="text-xl" />
+                Work Experience
+              </li>
+            </Link>
+            <Link to="/contact">
+              <li className="mt-5 flex flex-row gap-4 font-semibold items-center">
+                <FaHandPointRight className="text-xl" />
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
