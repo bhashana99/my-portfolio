361385d013ed16dbd7e10aeff5e44ba6e1a69131
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-16 16:37:27 +0530

    Re-Design Project page and remove create-project page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,7 +11,7 @@ import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import Setting from "./pages/Setting";
 import PrivateRoute from "./components/PrivateRoute";
-import CreateProject from "./pages/CreateProject";
+
 
 export default function App() {
   return (
@@ -19,17 +19,16 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route  element={<PrivateRoute />}>
+        <Route element={<PrivateRoute />}>
           <Route path="/edit" element={<Edit />} />
           <Route path="/basic-info" element={<BasicInfo />} />
           <Route path="/social-media" element={<SocialMedia />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/education" element={<Education/>} />
+          <Route path="/education" element={<Education />} />
           <Route path="/certificate" element={<Certificate />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/setting" element={<Setting />} />
-          <Route path="/create-project" element={<CreateProject />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- front-end/src/pages/Projects.jsx
+++ front-end/src/pages/Projects.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import { FaPlus } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 export default function Projects() {
+  const [showForm, setShowForm] = useState(false);
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
+
+  const handleAddButton = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div className="flex flex-col md:flex-row gap-3 bg-gray-300 min-h-screen">
       {/* sidebar */}
@@ -15,17 +24,90 @@ export default function Projects() {
           Projects
         </h1>
         <div className="mt-5  ">
-          <Link to="/create-project">
-            <div className="max-w-72 flex flex-row gap-5 border-dashed border-2 border-indigo-600 p-2  justify-center mx-auto hover:border-green-600 ">
+          {!showForm && (
+            <div
+              className="max-w-72 flex flex-row gap-5 border-dashed border-2 border-indigo-600 p-2  justify-center mx-auto hover:border-green-600 "
+              onClick={toggleForm}
+            >
               <FaPlus className="text-2xl text-blue-600 " />
               <h3>Add New Project</h3>
             </div>
-          </Link>
-
-          <div className="mt-5">
-            <h2 className="font-semibold text-xl md:ml-10">Recent Projects</h2>
-          </div>
+          )}
         </div>
+        {showForm && (
+        <div className="mt-5">
+          <form className="mt-5">
+            <div className="flex flex-col gap-2 mt-5">
+              <label htmlFor="projectName">
+                Project Name<span className="text-red-600 text-2xl">*</span>
+              </label>
+              <input
+                type="text"
+                placeholder="e.g. Portfolio"
+                id="projectName"
+                className="p-1"
+                required
+              />
+            </div>
+            <div className="flex flex-col gap-2 mt-5">
+              <label htmlFor="projectDescription">Description</label>
+              <textarea
+                rows={4}
+                type="text"
+                placeholder="e.g. Portfolio"
+                id="projectDescription"
+                className="p-1"
+              />
+            </div>
+            <div className="flex flex-col gap-2 mt-5">
+              <label htmlFor="projectDescription">
+                Images:
+                <span className="font-normal text-pink-600 ml-2">
+                  The first image will be the cover (max 6)
+                </span>
+              </label>
+              <div className="flex gap-4">
+                <input
+                  className="p-3 border border-gray-300 rounded w-full"
+                  type="file"
+                  id="images"
+                  accept="image/*"
+                  multiple
+                />
+                <button
+                  type="button"
+                  className=" px-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg "
+                >
+                  Upload
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-row gap-2 justify-end">
+              <div>
+                <button
+                  onClick={toggleForm}
+                  className="mt-5 p-3 bg-white border-black w-full text-black rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+                >
+                  Back
+                </button>
+              </div>
+              <div>
+                <button
+                  onClick={handleAddButton}
+                  className="mt-5 p-3 bg-green-700 w-full text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+                >
+                  ADD
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        )}
+         {!showForm && (
+        <div className="mt-5">
+          <h2 className="font-semibold text-xl md:ml-10">Recent Projects</h2>
+        </div>
+         )}
       </div>
     </div>
   );
