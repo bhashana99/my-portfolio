0e3bce2418209b3e1b0f5a4e654ca203fd9219b5
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-18 21:16:13 +0530

    create update contact info API Route

--- back-end/controllers/contact.controller.js
+++ back-end/controllers/contact.controller.js
@@ -20,3 +20,22 @@ export const getContactInfo = async (req, res, next) => {
         next(error);
     }
 }
+
+export const updateContactInfo = async (req, res, next) => {
+    const contactInfo = await ContactInfo.findById(req.params.id);
+
+    if (!contactInfo) {
+        return next(errorHandler(404, "Contact Info not found!"));
+    }
+
+    try {
+        const updateContactInfo = await ContactInfo.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        return res.status(200).json(updateContactInfo);
+    } catch (error) {
+        next(error);
+    }
+}
--- back-end/routes/contact.route.js
+++ back-end/routes/contact.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createContactInfo ,getContactInfo} from '../controllers/contact.controller.js'
+import { createContactInfo ,getContactInfo,updateContactInfo} from '../controllers/contact.controller.js'
 
 const router = express.Router();
 
 router.post('/create-contactInfo', createContactInfo);
 router.get('/get-contactInfo', getContactInfo);
+router.post('/update-contactInfo/:id', updateContactInfo);
 
 export default router;
