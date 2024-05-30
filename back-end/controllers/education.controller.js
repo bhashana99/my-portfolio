b14fb9ded45f52e8a460817e4673c62eb1df3e32
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-30 13:29:50 +0530

    create delete education API Route

--- back-end/controllers/education.controller.js
+++ back-end/controllers/education.controller.js
@@ -20,3 +20,17 @@ export const getEducations = async (req, res, next) => {
         next(error);
     }
 }
+
+export const deleteEducation = async (req, res, next) => {
+    try {
+        const education = await Education.findById(req.params.id);
+        if (!education) {
+            return res.status(404).json({ message: "Education not found!" });
+        }
+
+        await Education.findByIdAndDelete(req.params.id);
+        return res.status(200).json({ message: "Education deleted!" });
+    } catch (error) {
+        next(error);
+    }
+}
--- back-end/routes/education.route.js
+++ back-end/routes/education.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createEducation,getEducations } from '../controllers/education.controller.js';
+import { createEducation,getEducations ,deleteEducation} from '../controllers/education.controller.js';
 
 const router = express.Router();
 
 router.post('/create-education', createEducation);
 router.get('/get-educations', getEducations);
+router.delete('/delete-education/:id', deleteEducation);
 
 export default router;
