ab6f897bfc443f2eb19d9b93c4effe98435bb1ce
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-30 19:06:39 +0530

    complete functionality to fetch education data

--- front-end/src/pages/EditEducation.jsx
+++ front-end/src/pages/EditEducation.jsx
@@ -1,14 +1,16 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
-import { FaPlus } from "react-icons/fa";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function EditEducation() {
-    const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [educations, setEducations] = useState([]);
-  const [showEducationsError, setShowEducationsError] = useState(false);
+  const [initialFormData, setInitialFormData] = useState({});
+  const [isFormChanged, setIsFormChanged] = useState(false);
+
+  const params = useParams();
 
   const [formData, setFormData] = useState({
     school: "",
@@ -23,21 +25,53 @@ export default function EditEducation() {
     setShowForm(!showForm);
   };
 
-  const handleChange = (e) => {}
+  useEffect(() => {
+    const fetchEducation = async () => {
+      const eduId = params.educationId;
+      const res = await fetch(`/api/education/get-education/${eduId}`);
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      const formattedData = {
+        ...data,
+        startDate: new Date(data.startDate).toISOString().substring(0, 7),
+        endDate: new Date(data.endDate).toISOString().substring(0, 7),
+      };
+
+      setFormData(formattedData);
+      setInitialFormData(formattedData);
+    };
+
+    fetchEducation();
+  }, [params.educationId]);
+
+  useEffect(() => {
+    const hasFormChanged =
+      JSON.stringify(formData) !== JSON.stringify(initialFormData);
+    setIsFormChanged(hasFormChanged);
+  }, [formData, initialFormData]);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
 
   const handleSubmit = async (e) => {};
 
-
   return (
     <div className="flex flex-col md:flex-row gap-3 bg-gray-300 min-h-screen">
-    {/* sidebar */}
-    <div className="fixed top-0 left-0 h-full w-auto">
-      <Sidebar />
-    </div>
-    <div className="p-5 flex-1 md:ml-52">
-      <h1 className="text-center justify-center text-xl md:text-3xl font-bold ">
-        Edit Education
-      </h1>
+      {/* sidebar */}
+      <div className="fixed top-0 left-0 h-full w-auto">
+        <Sidebar />
+      </div>
+      <div className="p-5 flex-1 md:ml-52">
+        <h1 className="text-center justify-center text-xl md:text-3xl font-bold ">
+          Edit Education
+        </h1>
         <div className="mt-5">
           <form className="mt-5" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-2 mt-5">
@@ -50,7 +84,8 @@ export default function EditEducation() {
                 id="school"
                 className="p-1"
                 required
-                
+                value={formData.school}
+                onChange={handleChange}
               />
             </div>
             <div className="flex flex-col gap-2 mt-5">
@@ -63,7 +98,8 @@ export default function EditEducation() {
                 id="degreeName"
                 className="p-1"
                 required
-                
+                value={formData.degreeName}
+                onChange={handleChange}
               />
             </div>
             <div className="flex flex-row gap-5">
@@ -76,7 +112,8 @@ export default function EditEducation() {
                   id="startDate"
                   className="p-1"
                   required
-                  
+                  value={formData.startDate}
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex flex-col gap-2 mt-5">
@@ -88,7 +125,8 @@ export default function EditEducation() {
                   id="endDate"
                   className="p-1"
                   required
-                  
+                  value={formData.endDate}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -100,37 +138,27 @@ export default function EditEducation() {
                 className="p-1"
                 min="1.00"
                 max="4.00"
-              
                 step="0.01"
-                
-              
+                value={formData.gpa}
+                onChange={handleChange}
               />
             </div>
             <div className="flex flex-col gap-2 mt-5">
               <label htmlFor="description">Description</label>
-              <textarea
-                rows={4}
-                type="text"
-                id="description"
-                className="p-1"
-                
-              />
+              <textarea rows={4} type="text" id="description" className="p-1" />
             </div>
             <div className="flex flex-row gap-2 justify-end">
               <div>
                 <Link to="/education">
-                <button
-                
-                  className="mt-5 p-3 bg-white border-black w-full text-black rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-                >
-                  Back
-                </button>
+                  <button className="mt-5 p-3 bg-white border-black w-full text-black rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+                    Back
+                  </button>
                 </Link>
               </div>
               <div>
                 <button
-                disabled={loading}
-                className="mt-5 p-3 px-16 bg-green-700 w-full text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+                  disabled={loading}
+                  className="mt-5 p-3 px-16 bg-green-700 w-full text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
                 >
                   {loading ? "Editing..." : "Edit"}
                 </button>
@@ -139,9 +167,7 @@ export default function EditEducation() {
             {error && <p className="text-red-700">{error}</p>}
           </form>
         </div>
-      
-      
+      </div>
     </div>
-  </div>
-  )
+  );
 }
