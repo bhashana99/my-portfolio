ad2213c44ce8b77c78bb19a9c03aabe82d523db7
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-31 21:42:20 +0530

    create fetch all work experience API Route

--- back-end/controllers/work.controller.js
+++ back-end/controllers/work.controller.js
@@ -7,4 +7,16 @@ export const createWork = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
+
+export const getWorks = async (req, res, next) => {
+  try {
+    const works = await Work.find();
+    if (!works) {
+      return res.status(404).json({ message: "Works not found!" });
+    }
+    return res.status(200).json(works);
+  } catch (error) {
+    next(error);
+  }
+};
--- back-end/routes/work.route.js
+++ back-end/routes/work.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createWork } from "../controllers/work.controller.js";
+import { createWork,getWorks } from "../controllers/work.controller.js";
 
 const router =  express.Router();
 
 router.post("/create-work", createWork);
+router.get("/get-works", getWorks);
 
 export default router;
