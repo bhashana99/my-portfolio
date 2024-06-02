4b9161041c6a6f8176c2ca3795444f5370d09b1f
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-06-02 12:47:11 +0530

    add loading animation to welcome component

--- front-end/src/components/Welcome.jsx
+++ front-end/src/components/Welcome.jsx
@@ -3,14 +3,15 @@ import { CiHome } from "react-icons/ci";
 
 export default function Main() {
   const [basicInfo, setBasicInfo] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchBasicInfo = async () => {
       try {
         const res = await fetch("/api/basicInfo/get-basicInfo");
         const data = await res.json();
-        // console.log(data);
         setBasicInfo(data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -20,33 +21,38 @@ export default function Main() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-slate-800 flex md:items-center justify-center ">
-      {basicInfo && (
-        <div className="flex flex-col md:grid md:grid-cols-3 text-center  gap-10">
-          <div className="">
+    <div className="min-h-screen bg-slate-800 flex items-center justify-center">
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <div className="w-16 h-16 border-4 border-t-4 border-t-transparent border-white rounded-full animate-spin"></div>
+        </div>
+      ) : (
+        <div className="flex flex-col md:grid md:grid-cols-3 text-center gap-10">
+          <div>
             <img
               src={basicInfo.profileImage}
               alt="profile image"
-              className="rounded-full h-80 w-80  object-cover self-center border-double border-blue-400 border-8"
+              className="rounded-full h-80 w-80 object-cover self-center border-double border-blue-400 border-8"
             />
           </div>
           <div className="col-span-2 text-start">
-            
-              <p className="text-3xl font-semibold font-mono">Hi There,</p>
-              <h1 className="mt-5 text-4xl font-bold font-mono tracking-widest  ">
-                {"I'm "}
-                {basicInfo.firstName} {basicInfo.lastName}
-                {basicInfo.additionalName && (
-                    <span className="text-sm"> {" ( "} {basicInfo.additionalName} {" ) "}</span>
-                )}
-              </h1>
-              <p>{basicInfo.headline}</p>
-              <p className="">{basicInfo.about}</p>
-              <div className="flex flex-row items-center">
+            <p className="text-3xl font-semibold font-mono">Hi There,</p>
+            <h1 className="mt-5 text-4xl font-bold font-mono tracking-widest">
+              {"I'm "} {basicInfo.firstName} {basicInfo.lastName}
+              {basicInfo.additionalName && (
+                <span className="text-sm">
+                  {" ( "} {basicInfo.additionalName} {" ) "}
+                </span>
+              )}
+            </h1>
+            <p>{basicInfo.headline}</p>
+            <p>{basicInfo.about}</p>
+            <div className="flex flex-row items-center">
               <CiHome />
-              <p>{basicInfo.city}{","}{basicInfo.country}</p>
-              </div>
-           
+              <p>
+                {basicInfo.city}, {basicInfo.country}
+              </p>
+            </div>
           </div>
         </div>
       )}
