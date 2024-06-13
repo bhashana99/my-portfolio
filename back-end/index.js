cd551de4b4b17c97366e48476286858b30b5946a
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-06-14 00:10:15 +0530

    add pre installation

--- back-end/index.js
+++ back-end/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import basicInfoRouter from "./routes/basicInfo.route.js"; 
+import basicInfoRouter from "./routes/basicInfo.route.js";
 import socialMediaRouter from "./routes/socialMedia.route.js";
 import contactInfoRouter from "./routes/contact.route.js";
 import projectRouter from "./routes/project.route.js";
 import educationRouter from "./routes/education.route.js";
 import certificateRouter from "./routes/certificate.route.js";
 import workRouter from "./routes/work.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -21,6 +22,8 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -29,14 +32,20 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.use("/api/user",userRouter);
+app.use("/api/user", userRouter);
 app.use("/api/basicInfo", basicInfoRouter);
 app.use("/api/socialMedia", socialMediaRouter);
 app.use("/api/contactInfo", contactInfoRouter);
-app.use("/api/project",projectRouter);
-app.use("/api/education",educationRouter);
-app.use("/api/certificate",certificateRouter);
-app.use("/api/work",workRouter);
+app.use("/api/project", projectRouter);
+app.use("/api/education", educationRouter);
+app.use("/api/certificate", certificateRouter);
+app.use("/api/work", workRouter);
+
+app.use(express.static(path.join(__dirname, "/front-end/dist")));
+
+app.get("*", (req, res) =>{
+  res.sendFile(path.join(__dirname, "front-end", "dist", "index.html"))
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
